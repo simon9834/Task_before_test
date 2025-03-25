1d1771d7838eef6159fb094bb54a3c8999068840
Šimon Juda Hloska <hloska@spsejecna.cz>
2025-03-25 15:52:33 +0100

    Add project files.

---
+++ Task_before_test/Pantry.cs
@@ -0,0 +1,38 @@
+﻿public class Pantry
+{
+    private int herbCount;
+    private int crystalCount;
+    private int essencesCount;
+
+    private int maxHerbCount;
+    private int maxCrystalCount;
+    private int maxEssences;
+
+    public void refillMaterials(int herbs, int crystals, int essences)
+    {
+        if(checkMaterialMax(herbs, herbCount, maxHerbCount) &&
+        checkMaterialMax(crystals, crystalCount, maxCrystalCount) &&
+        checkMaterialMax(essences, essencesCount, maxEssences))
+        {
+            herbCount += herbs;
+            crystalCount += crystals;
+            essencesCount += essences;
+        }
+        else
+        {
+            Console.WriteLine("Ur delivery sadly exceeded our maximum storage space");
+        }
+    }
+    public bool checkMaterialMax(int item, int baseItem, int itemMax)
+    {
+        if(baseItem + item <= itemMax)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void brewInvisPot()
+    {
+
+    }
+}
