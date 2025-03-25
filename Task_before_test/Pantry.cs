9b077771f35f6e63f5f405b5f8fd21a4dd5cc958
Šimon Juda Hloska <hloska@spsejecna.cz>
2025-03-25 16:51:34 +0100

    Done all in excercise

--- Task_before_test/Pantry.cs
+++ Task_before_test/Pantry.cs
@@ -4,35 +4,102 @@
     private int crystalCount;
     private int essencesCount;
 
+    public int HerbCount
+    {
+        get { return herbCount; }
+        set { herbCount = value; }
+    }
+
+    public int CrystalCount
+    {
+        get { return crystalCount; }
+        set { crystalCount = value; }
+    }
+
+    public int EssencesCount
+    {
+        get { return essencesCount; }
+        set { essencesCount = value; }
+    }
+
     private int maxHerbCount;
     private int maxCrystalCount;
     private int maxEssences;
+    private readonly object lockObj = new object();
+
+    public Pantry(int maxHerbCount, int maxCrystalCount, int maxEssences)
+    {
+        this.maxHerbCount = maxHerbCount;
+        this.maxCrystalCount = maxCrystalCount;
+        this.maxEssences = maxEssences;
+    }
 
     public void refillMaterials(int herbs, int crystals, int essences)
     {
-        if(checkMaterialMax(herbs, herbCount, maxHerbCount) &&
-        checkMaterialMax(crystals, crystalCount, maxCrystalCount) &&
-        checkMaterialMax(essences, essencesCount, maxEssences))
+        lock (lockObj)
         {
-            herbCount += herbs;
-            crystalCount += crystals;
-            essencesCount += essences;
-        }
-        else
-        {
-            Console.WriteLine("Ur delivery sadly exceeded our maximum storage space");
+            if (checkMaterialMax(herbs, herbCount, maxHerbCount) &&
+            checkMaterialMax(crystals, crystalCount, maxCrystalCount) &&
+            checkMaterialMax(essences, essencesCount, maxEssences))
+            {
+                herbCount += herbs;
+                crystalCount += crystals;
+                essencesCount += essences;
+                Console.WriteLine("done delivery");
+            }
+            else
+            {
+                Console.WriteLine("Ur delivery sadly exceeded our maximum storage space");
+            }
         }
     }
-    public bool checkMaterialMax(int item, int baseItem, int itemMax)
+    private bool checkMaterialMax(int item, int baseItem, int itemMax)
     {
-        if(baseItem + item <= itemMax)
+        if (baseItem + item <= itemMax)
         {
             return true;
         }
         return false;
     }
-    public void brewInvisPot()
+    public bool brewInvisPot()
     {
-
+        lock (lockObj)
+        {
+            if (checkAvailability(herbCount, 2) &&
+        checkAvailability(crystalCount, 3) &&
+        checkAvailability(essencesCount, 2))
+            {
+                substractItemsForPot(ref herbCount, 2);
+                substractItemsForPot(ref crystalCount, 3);
+                substractItemsForPot(ref essencesCount, 2);
+                return true;
+            }
+            return false;
+        }
+    }
+    public bool brewStrengthPot()
+    {
+        lock (lockObj)
+        {
+            if (checkAvailability(herbCount, 5) &&
+        checkAvailability(crystalCount, 2) &&
+        checkAvailability(essencesCount, 1))
+            {
+                substractItemsForPot(ref herbCount, 5);
+                substractItemsForPot(ref crystalCount, 2);
+                substractItemsForPot(ref essencesCount, 1);
+                return true;
+            }
+            return false;
+        }
+    }
+    private bool checkAvailability(int item, int desiredCountOfItem)
+    {
+        if (item >= desiredCountOfItem) return true;
+        return false;
+    }
+    private void substractItemsForPot(ref int item, int substractCount)
+    {
+        item -= substractCount;
     }
 }
--- Task_before_test/Program.cs
+++ Task_before_test/Program.cs
@@ -1,1 +1,90 @@
-﻿
+﻿using System.Runtime.InteropServices;
+
+Pantry pa = new Pantry(50, 30, 20);
+int herbs;
+int crystals;
+int essencess;
+int brewOfStrengthCount = 0;
+int brewOfInvisibilityCount = 0;
+Random ra = new Random();
+
+Task supplies = Task.Run(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        herbs = ra.Next(0, 11);
+        crystals = ra.Next(0, 11);
+        essencess = ra.Next(0, 11);
+        pa.refillMaterials(herbs, crystals, essencess);
+    }
+    Console.WriteLine("SUP all done");
+});
+await supplies;
+Task witchCrafts = Task.Run(() =>
+{
+    bool res = true;
+    for (int i = 0; i < 5; i++)
+    {
+        res = pa.brewStrengthPot();
+        Console.WriteLine($"Witch done? : {res.ToString()}");
+        if (res)
+        {
+            brewOfStrengthCount++;
+        }
+    }
+    Console.WriteLine("WI all done");
+});
+await witchCrafts;
+Task alchemistCrafting = Task.Run(() =>
+{
+    bool res = true;
+    for (int i = 0; i < 4; i++)
+    {
+        res = pa.brewInvisPot();
+        Console.WriteLine($"Alchemist done? : {res.ToString()}");
+        if (res)
+        {
+            brewOfInvisibilityCount++;
+        }
+    }
+    Console.WriteLine("AL1 all done");
+});
+await alchemistCrafting;
+Task randomAlchemist = Task.Run(() =>
+{
+    bool res = true;
+    int randomRoundNum = ra.Next(1, 11);
+    for (int i = 0; i < randomRoundNum; i++)
+    {
+        if (ra.Next(0, 2) == 1)
+        {
+            res = pa.brewInvisPot();
+            Console.WriteLine($"Alchemist done? : {res.ToString()}");
+            if (res)
+            {
+                brewOfInvisibilityCount++;
+            }
+        }
+        else
+        {
+            res = pa.brewStrengthPot();
+            Console.WriteLine($"Alchemist done? : {res.ToString()}");
+            if (res)
+            {
+                brewOfStrengthCount++;
+            }
+        }
+    }
+    Console.WriteLine("AL2 all done");
+});
+
+await Task.WhenAll(supplies, witchCrafts, alchemistCrafting, randomAlchemist);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Strength potions count: {brewOfStrengthCount}!");
+Console.WriteLine($"Invisibility potions count: {brewOfInvisibilityCount}!");
+Console.WriteLine();
+Console.WriteLine($"crystals: {pa.CrystalCount}, herbs: {pa.HerbCount}, essencess: {pa.EssencesCount}");
+
+
